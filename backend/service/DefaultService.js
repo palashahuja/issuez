a3a7da22cb9e3fef9555a05f16d859d7f5090119
palashahuja <ahujapalash1994@gmail.com>
2020-05-01 00:53:13 -0400

    adding changes for getting the possible issue list

--- backend/service/DefaultService.js
+++ backend/service/DefaultService.js
@@ -60,7 +60,7 @@ exports.assignUserToIssue = function(issueid,userid) {
  **/
 exports.checkUserProjectLead = function(projectid,userid) {
   return new Promise((resolve, reject) => {
-    let query_str = 'SELECT * FROM `issue_tracker`.`leads` where project_id = ? and user_id = ?';
+    let query_str = 'SELECT * FROM `leads` where project_id = ? and user_id = ?';
     let params = [projectid, userid];
     Connection.query(query_str, params, (error, results, fields) => {
       if(error){
@@ -146,27 +146,24 @@ exports.createProjectLead = function(projectid,userid) {
  **/
 exports.getIssueAssignedUser = function(userid) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "project_id" : "project_id",
-  "reporter_id" : "reporter_id",
-  "description" : "description",
-  "created_date" : "created_date",
-  "title" : "title",
-  "latest_status" : "latest_status"
-}, {
-  "project_id" : "project_id",
-  "reporter_id" : "reporter_id",
-  "description" : "description",
-  "created_date" : "created_date",
-  "title" : "title",
-  "latest_status" : "latest_status"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+       let query_str = " WITH a AS \
+       ( SELECT a.issue_id, max(c.updated_time) AS latest_time FROM  \
+        `issue_tracker`.`issues` a LEFT JOIN `issue_tracker`.`assigned` b \
+        ON a.issue_id = b.issue_id LEFT JOIN `issue_tracker`.`issue_history` c \
+        ON a.issue_id = c.issue_id WHERE  b.user_id = ? GROUP BY  a.issue_id) \
+        SELECT b.*, a.latest_time, c.status FROM a LEFT JOIN `issue_tracker`.`issues` b \
+        ON a.issue_id = b.issue_id LEFT JOIN `issue_tracker`.`issue_history` c ON \
+        a.issue_id = c.issue_id AND c.updated_time = a.latest_time";
+      let params = [userid];
+      Connection.query(query_str, params, (error, results, fields) => {
+        if(error){
+          reject({'message': error});
+        }
+        else {
+          console.log(results);
+          resolve(results);
+        }
+      })
   });
 }
 
@@ -179,20 +176,16 @@ exports.getIssueAssignedUser = function(userid) {
  **/
 exports.getIssueDetails = function(issueid) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "project_id" : "project_id",
-  "reporter_id" : "reporter_id",
-  "description" : "description",
-  "created_date" : "created_date",
-  "title" : "title",
-  "latest_status" : "latest_status"
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    let query_str = 'select * from `issues` where `issue_id` = ?';
+    let params = [issueid];
+    Connection.query(query_str, params, (error, results, fields) => {
+      if(error) {
+        reject({'message': error})
+      }
+      else {
+        resolve(results);
+      }
+    })
   });
 }
 
@@ -205,21 +198,21 @@ exports.getIssueDetails = function(issueid) {
  **/
 exports.getIssueHistoryDetails = function(issueid) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "updated_time" : "updated_time",
-  "issue_id" : "issue_id",
-  "status" : "status"
-}, {
-  "updated_time" : "updated_time",
-  "issue_id" : "issue_id",
-  "status" : "status"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+//     var examples = {};
+//     examples['application/json'] = [ {
+//   "updated_time" : "updated_time",
+//   "issue_id" : "issue_id",
+//   "status" : "status"
+// }, {
+//   "updated_time" : "updated_time",
+//   "issue_id" : "issue_id",
+//   "status" : "status"
+// } ];
+//     if (Object.keys(examples).length > 0) {
+//       resolve(examples[Object.keys(examples)[0]]);
+//     } else {
+//       resolve();
+//     }
   });
 }
 
@@ -274,21 +267,16 @@ exports.getNextPossibleStatus = function(projectid,status) {
  **/
 exports.getProjectDetails = function(projectid) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "name" : "name",
-  "description" : "description",
-  "created_date" : "created_date"
-}, {
-  "name" : "name",
-  "description" : "description",
-  "created_date" : "created_date"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    let query_str = 'SELECT name, description, created_date from `project` where project_id = ?'
+    let params = [projectid]
+    Connection.query(query_str, params, (error, results, fields) => {
+      if(error){
+        reject({'message': error});
+      }
+      else {
+        resolve(results);
+      }
+    })
   });
 }
 
